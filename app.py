c180c2dcae68237a06cc944df30e3b254031cebf
anushka81 <119094969+anushka81@users.noreply.github.com>
2024-02-03 01:02:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import gradio as gr
 def style(x): return x[0].isupper()
 
 
-learn=load_learner('C:\Users\Anush\Downloads\model.pkl')
+learn=load_learner('https://drive.google.com/file/d/1jjGAKMHxvL7xJIHXFXLBULOmln9eoavW/view?usp=sharing')
 
 categories = ('active wear','bussiness wear','ethnic wear','goth fashion')
 def classify_image(img):
@@ -13,6 +13,6 @@ def classify_image(img):
 
 image = gr.inputs. Image (shape= (192, 192))
 label = gr.outputs. Label()
-examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
+# examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
 intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 intf.launch(inline=False)
