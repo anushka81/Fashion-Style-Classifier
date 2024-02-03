36ed063dc12a9f00d2fdb5f891f86ab157ebe527
anushka81 <119094969+anushka81@users.noreply.github.com>
2024-02-04 03:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
+
 from fastai.vision.all import *
 import gradio as gr
+from PIL import Image
 def style(x): return x[0].isupper()
 
 
-learn=load_learner('https://drive.google.com/file/d/1jjGAKMHxvL7xJIHXFXLBULOmln9eoavW/view?usp=sharing')
+learn = load_learner('model.pkl')
 
 categories = ('active wear','bussiness wear','ethnic wear','goth fashion')
+
 def classify_image(img):
-    
     pred, idx, probs = learn.predict(img)
+    img = img.resize((192, 192))
+    # Your classification logic here
     return dict(zip(categories,map (float,probs)))
 
-image = gr.inputs. Image (shape= (192, 192))
-label = gr.outputs. Label()
-# examples = ['/kaggle/input/ip-pics/goth.jpg','/kaggle/input/bizzzzzzzzzz/bizzzzzzz.jpg','/kaggle/input/piccccc/ethnic.jpg' ]
-intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+# image = gr.inputs.Image(shape=(192, 192))
+# label = gr.outputs.Label()
+
+# intf = gr. Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+
+intf = gr.Interface(
+    fn=classify_image,
+    inputs=gr.Image(type="pil"),
+    outputs=gr.Label()
+    # examples=['business.jpg']
+)
+
 intf.launch(inline=False)
